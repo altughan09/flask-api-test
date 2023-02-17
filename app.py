b5c0650f1a86310de2f570663855c951a85df362
altughan.ozengi <altughan.ozengi@pl.abb.com>
2023-02-17 13:21:30 +0100

    added host

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, jsonify
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     list = [
-            {'a': 1, 'b': 2},
-            {'a': 5, 'b': 10}
-           ]
+        {'a': 1, 'b': 2},
+        {'a': 5, 'b': 10}
+    ]
     return jsonify(list)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
