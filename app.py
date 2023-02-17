f9e726dd2d3deab5ca4cf4647e25fcb1b68b247c
altughan.ozengi <altughan.ozengi@pl.abb.com>
2023-02-17 14:17:44 +0100

    m

--- app.py
+++ app.py
@@ -2,13 +2,9 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    list = [
-        {'a': 1, 'b': 2},
-        {'a': 5, 'b': 10}
-    ]
-    return jsonify(list)
+@app.route('/health')
+def health_check():
+    return jsonify({'status': 'success'})
 
 
 if __name__ == "__main__":
